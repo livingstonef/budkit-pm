e3ab1ab096f048511aded3d376575e5c8cc4fd84
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-16 21:00:05 +0100

    Adds the project and task controllers

---
+++ src/Provider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Project;
+
+use Budkit\Application\Support\Service;
+use Budkit\Dependency\Container;
+
+class Provider implements Service
+{
+
+    protected $application;
+
+    public function __construct(Container $application)
+    {
+        $this->application = $application;
+    }
+
+    public function onRegister()
+    {
+        $app = $this->application;
+        //Can Include Routes; using $this->application->route->add() or simply \Route::add().
+        $app->route
+            //->setResourceCallable("App\\Calendar\\Calendar")
+            ->attachResource("/calendar", "App\\Calendar\\Calendar"); //collection of events
+
+        //Projects plugin;
+        $app->route->add("/projects{format}", "projects", "App\\Project\\Controller\\Project");
+        $app->route->attachResource("/project", "App\\Project\\Controller\\Project"); //multiple projects types and status e.g project proposal
+        $app->route->attachResource("/task", "App\\Project\\Controller\\Task"); //tasks? tasks are a collection of protocols mark task as results?
+
+    }
+
+    public function definition()
+    {
+        return array("app.register" => "onRegister");
+    }
+
+
+}
