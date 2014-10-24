ca2eab7ac7841d46213f2ab6f8b3beaa60e0c44f
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-24 11:45:29 +0100

    Coding style cleanup

--- src/Provider.php
+++ src/Provider.php
@@ -5,18 +5,15 @@ namespace App\Project;
 use Budkit\Application\Support\Service;
 use Budkit\Dependency\Container;
 
-class Provider implements Service
-{
+class Provider implements Service {
 
     protected $application;
 
-    public function __construct(Container $application)
-    {
+    public function __construct(Container $application) {
         $this->application = $application;
     }
 
-    public function onRegister()
-    {
+    public function onRegister() {
         $app = $this->application;
         //Can Include Routes; using $this->application->route->add() or simply \Route::add().
         $app->route
@@ -25,15 +22,16 @@ class Provider implements Service
 
         //Projects plugin;
         $app->route->add("/projects{format}", "projects", "App\\Project\\Controller\\Project");
-        $app->route->attachResource("/project", "App\\Project\\Controller\\Project"); //multiple projects types and status e.g project proposal
-        $app->route->attachResource("/task", "App\\Project\\Controller\\Task"); //tasks? tasks are a collection of protocols mark task as results?
+
+        //multiple projects types and status e.g project proposal
+        $app->route->attachResource("/project","App\\Project\\Controller\\Project");
+
+        //tasks? tasks are a collection of protocols mark task as results?
+        $app->route->attachResource("/task", "App\\Project\\Controller\\Task");
 
     }
 
-    public function definition()
-    {
-        return array("app.register" => "onRegister");
+    public function definition() {
+        return ["app.register" => "onRegister"];
     }
-
-
 }
